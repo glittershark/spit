6cccf38e1a0b94f40dfb56d4fd57ac82c4044bba
Aspen Smith <root@gws.fyi>
2024-01-28 15:25:09 -0500

    lambda and quote in the stdlib

--- lib/lisp/stdlib.lisp
+++ lib/lisp/stdlib.lisp
@@ -2,18 +2,17 @@
 
 ;; (defmacro* name arg ret)
 ;; -> (list (.def name (.lambda arg ret)) (.make-macro name))
-(.def
- defmacro*
- (.lambda
-  args
-  (list
-   'list
-   (list
-    '.def
-    (car args)
-    (list
-     '.lambda
-     (car (cdr args))
-     (car (cdr (cdr args)))))
-   (list '.make-macro (car args)))))
+(.def defmacro*
+      (.lambda
+       (name args ret)
+       (list 'list
+             (list '.def name (list '.lambda args ret))
+             (list '.make-macro name))))
 (.make-macro defmacro*)
+
+(defmacro* defmacro-alias
+  (name alias-for)
+  (list 'defmacro* name 'args (list 'cons (list '.quote alias-for) 'args)))
+
+(defmacro-alias lambda .lambda)
+(defmacro-alias quote .quote)
