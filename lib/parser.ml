7aae80447fab4f1838426d148e6fe51c9ca253e1
Aspen Smith <root@gws.fyi>
2024-02-07 11:21:43 -0500

    Use AST pretty-printing for parser tests

--- lib/parser.ml
+++ lib/parser.ml
@@ -14,85 +14,74 @@ include Nice_parser.Make (struct
 let%test_module _ =
   (module struct
     let () = Printexc.record_backtrace false
-    let print_sexp s = Printf.printf !"%{sexp:Ast.Sexp.t}" s
 
     let parse_and_print s =
       let toplevels = parse_string s in
       if not (List.length toplevels = 1) then failwith "Found multiple toplevels";
-      List.hd_exn toplevels |> print_sexp
+      List.hd_exn toplevels |> Ast.Sexp.to_string |> print_endline
     ;;
 
     let%expect_test "atom" =
       parse_and_print "this_is_An_At0m";
-      [%expect {| (Atom this_is_An_At0m) |}]
+      [%expect {| this_is_An_At0m |}]
     ;;
 
     let%expect_test "list" =
       parse_and_print "(a bBBb)";
-      [%expect {| (List ((Atom a) (Atom bBBb))) |}]
+      [%expect {| (a bBBb) |}]
     ;;
 
     let%expect_test "nested list" =
       parse_and_print "\n(a x (y Zzz (q)) (e)\n\t   )    ";
-      [%expect
-        {|
-      (List
-       ((Atom a) (Atom x) (List ((Atom y) (Atom Zzz) (List ((Atom q)))))
-        (List ((Atom e))))) |}]
+      [%expect {|
+      (a x (y Zzz (q)) (e)) |}]
     ;;
 
     let%expect_test "empty list" =
       parse_and_print "( () (  ) (\t) (\n\t))";
-      [%expect {| (List ((List ()) (List ()) (List ()) (List ()))) |}]
+      [%expect {| (() () () ()) |}]
     ;;
 
     let%expect_test "cons" =
       parse_and_print "(x . y)";
-      [%expect {| (Cons ((Atom x) (Atom y))) |}]
+      [%expect {| (x . y) |}]
     ;;
 
     let%expect_test "string literals" =
       parse_and_print {| "s" |};
-      [%expect {|(Literal (String s))|}];
+      [%expect {|"s"|}];
       parse_and_print {| "\n" |};
-      [%expect {|(Literal (String "\n"))|}];
+      [%expect {|
+        "
+        "|}];
       parse_and_print {| "\"" |};
-      [%expect {|(Literal (String "\""))|}]
+      [%expect {|"\""|}]
     ;;
 
     let%expect_test "integer literals" =
       parse_and_print "123";
-      [%expect {|(Literal (Int 123))|}];
+      [%expect {|123|}];
       parse_and_print "-123";
-      [%expect {|(Literal (Int -123))|}]
+      [%expect {|-123|}]
     ;;
 
     let%expect_test "many literals" =
       parse_and_print {|
       (1 2 (s "three" four))
     |};
-      [%expect
-        {|
-      (List
-       ((Literal (Int 1)) (Literal (Int 2))
-        (List ((Atom s) (Literal (String three)) (Atom four))))) |}]
+      [%expect {|
+      (1 2 (s "three" four)) |}]
     ;;
 
     let%expect_test "fancy atoms" =
       parse_and_print "(+ (/ 4 5 (* 7 8)) 9 (namespace/do-a-thing 10))";
-      [%expect
-        {|
-      (List
-       ((Atom +)
-        (List
-         ((Atom /) (Literal (Int 4)) (Literal (Int 5))
-          (List ((Atom *) (Literal (Int 7)) (Literal (Int 8))))))
-        (Literal (Int 9)) (List ((Atom namespace/do-a-thing) (Literal (Int 10)))))) |}]
+      [%expect {|
+      (+ (/ 4 5 (* 7 8)) 9 (namespace/do-a-thing 10)) |}]
     ;;
 
     let%expect_test "quote" =
       parse_and_print "'(foo 'bar)";
-      [%expect {| (Quote (List ((Atom foo) (Quote (Atom bar))))) |}]
+      [%expect {| '(foo 'bar) |}]
     ;;
 
     let%expect_test "comments" =
@@ -103,20 +92,15 @@ let%test_module _ =
           ; with a comment
         x) ; and another comment
       |};
-      [%expect {| (List ((Atom .lambda) (Atom x) (Atom x))) |}]
+      [%expect {| (.lambda x x) |}]
     ;;
 
     let%expect_test "quasiquote" =
       parse_and_print "`(1)";
-      [%expect {| (Quasiquote (List ((Literal (Int 1))))) |}];
+      [%expect {| `(1) |}];
       parse_and_print "`(1 ,(+ 1 1) ,@(list 3 4))";
-      [%expect
-        {|
-        (Quasiquote
-         (List
-          ((Literal (Int 1))
-           (Unquote (List ((Atom +) (Literal (Int 1)) (Literal (Int 1)))))
-           (UnquoteSplicing (List ((Atom list) (Literal (Int 3)) (Literal (Int 4)))))))) |}]
+      [%expect {|
+        `(1 ,(+ 1 1) ,@(list 3 4)) |}]
     ;;
 
     (* Failed parsing *)
